a408eedf876e5f930ec6dd01f2b441e6fcb33c73
igvafoto <igvafoto@gmail.com>
2026-07-17 18:02:49 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,44 @@
+var CACHE_NAME = "yearblocks-v1";
+var ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./apple-touch-icon.png",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(e) {
+  e.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(n) { return n !== CACHE_NAME; }).map(function(n) { return caches.delete(n); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(resp) {
+      if (resp) return resp;
+      return fetch(e.request).then(function(r) {
+        var clone = r.clone();
+        caches.open(CACHE_NAME).then(function(c) { c.put(e.request, clone); });
+        return r;
+      }).catch(function() {
+        return caches.match("./index.html");
+      });
+    })
+  );
+});
